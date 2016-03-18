f978a5ce7bbf8a2be41f400a4c91ce4ba5398db1
Charles Carter <charles.h.carter@gmail.com>
2016-03-18 10:13:54 -0700

    integrating api call into site

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,8 +1,12 @@
 var getRepos = require.('./../js/github.js').getRepos;
+var githubKey = require('./../.env').githubKey;
+// exports.username = user;
 
 $(document).ready(function() {
   ('#repoInfo').click(function() {
-    var user = $('#username').val();
-    $('#username').val("");
+    var user = $('#usernameRepos').val();
+    $('#usernameRepos').val("");
+    .show('#showRepos');
+
   });
 });
--- js/github.js
+++ js/github.js
@@ -1,12 +1,11 @@
 var githubKey = require('./../.env').githubKey;
+// var username = require('./../js/broswer-interface.js').username;
 
-$(document).ready(function() {
-    exports.getRepos = function(){
-      $.get('https://api.github.com/users/daneden?access_token=' + githubKey).then(function(response){
-        console.log(response);
-      }).fail(function(error){
-        console.log(error.responseJSON.message);
-      });
-    };
+
+exports.getRepos = function(username){
+  $.get('https://api.github.com/users/daneden?access_token=' + githubKey).then(function(response){
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
   });
-});
+};
